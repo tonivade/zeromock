3eeb40375211db6c131866a1781e4a39df318cf1
Antonio Muñoz <antonio.munoz@rumbo.com>
2018-02-23 14:57:51 +0100

    added new Bytes test
    array cannot be null

--- src/test/java/com/github/tonivade/zeromock/BytesTest.java
+++ src/test/java/com/github/tonivade/zeromock/BytesTest.java
@@ -12,6 +12,7 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.DynamicTest.dynamicTest;
 
@@ -27,6 +28,10 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestFactory;
 
 public class BytesTest {
+  @Test
+  public void notNull() {
+    assertThrows(NullPointerException.class, () -> fromArray(null));
+  }
   
   @Test
   public void bytesProperties() {
