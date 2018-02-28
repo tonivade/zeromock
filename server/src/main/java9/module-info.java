4115cbfa00160a43cbf25172244e06cb7fd65df7
Antonio Muñoz <antonio.munoz@rumbo.com>
2018-02-28 08:36:21 +0100

    added java9 module-info definitions

---
+++ server/src/main/java9/module-info.java
@@ -0,0 +1,5 @@
+module com.github.tonivade.zeromock.server {
+  requires com.github.tonivade.zeromock.core;
+  
+  exports com.github.tonivade.zeromock.server;
+}
---
+++ test/junit4/src/main/java9/module-info.java
@@ -0,0 +1,5 @@
+module com.github.tonivade.zeromock.junit4 {
+  requires com.github.tonivade.zeromock.server;
+  
+  exports com.github.tonivade.zeromock.junit4;
+}
