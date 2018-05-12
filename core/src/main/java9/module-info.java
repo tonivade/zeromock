be25d6a0171c484b79fe0ef38e91821ee43ef659
Antonio Muñoz <antoniogmc@gmail.com>
2018-05-12 11:45:49 +0200

    update module-info.java

--- core/src/main/java9/module-info.java
+++ core/src/main/java9/module-info.java
@@ -1,8 +1,8 @@
 module com.github.tonivade.zeromock.core {
   requires jdk.httpserver;
   requires java.xml.bind;
-  requires com.google.code.gson;
-  requires com.github.tonivade.equalizer;
+  requires gson;
+  requires equalizer;
   
   exports com.github.tonivade.zeromock.core;
 }
