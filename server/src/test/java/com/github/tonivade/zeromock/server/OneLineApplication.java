4ba60391f4ffc50c0c18f78b0702a4d17f36c277
Antonio Muñoz <antonio.munoz@rumbo.com>
2018-06-06 12:26:41 +0200

    added a simple test server to demostrate how easy is run it

---
+++ server/src/test/java/com/github/tonivade/zeromock/server/OneLineApplication.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2018, Antonio Gabriel Muñoz Conejo <antoniogmc at gmail dot com>
+ * Distributed under the terms of the MIT License
+ */
+package com.github.tonivade.zeromock.server;
+
+import static com.github.tonivade.zeromock.api.Handlers.ok;
+import static com.github.tonivade.zeromock.api.Matchers.get;
+import static com.github.tonivade.zeromock.server.MockHttpServer.listenAt;
+
+public class OneLineApplication {
+
+  public static void main(String[] args) {
+    listenAt(8080)
+        .when(get("/ping")).then(ok("pong"))
+        .start();
+  }
+}
