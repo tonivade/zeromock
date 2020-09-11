12693ba4b684c02be5bd146a70b6a508cb5ada69
Antonio Muñoz <antonio.munoz@clarity.ai>
2020-09-11 16:41:33 +0200

    mmmmm

---
+++ client/src/main/java/com/github/tonivade/zeromock/client/HttpClientOf.java
@@ -0,0 +1,9 @@
+package com.github.tonivade.zeromock.client;
+
+import com.github.tonivade.purefun.Witness;
+
+interface HttpClientOf<F extends Witness> {
+
+  // XXX: it would be nice to define here the method request, but for HttpClient that
+  // returns a naked HttpResponse it would be weird to return an Id<HttpResponse>
+}
