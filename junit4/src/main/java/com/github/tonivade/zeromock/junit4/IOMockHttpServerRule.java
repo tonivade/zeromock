8b69976f5e3488d46799c60453aaa2fd49f08358
Antonio Muñoz <antoniogmc@gmail.com>
2024-05-05 16:59:26 +0200

    update copyright

--- junit4/src/main/java/com/github/tonivade/zeromock/junit4/IOMockHttpServerRule.java
+++ junit4/src/main/java/com/github/tonivade/zeromock/junit4/IOMockHttpServerRule.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018-2023, Antonio Gabriel Muñoz Conejo <me at tonivade dot es>
+ * Copyright (c) 2018-2024, Antonio Gabriel Muñoz Conejo <me at tonivade dot es>
  * Distributed under the terms of the MIT License
  */
 package com.github.tonivade.zeromock.junit4;
